48cd2c7e6549418ce90dd00fa590ab0ca2e9ec68
Cristian20044121 <crdcaro01@gmail.com>
2025-02-28 16:19:12 -0500

    refactor createCounter to makeCounter for improved functionality and clarity

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,19 +1,12 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
-var createCounter = function (n) {
-  let call = (number) => {
-    let array = [];
-    array.push(number);
-    for (let i = 0; i <= array.length; i++) {
-       array.push(i + 1);
-       console.log(array);
-    }
-  };
-  return function () {
-    const result = call(n);
-    return result;
-  };
-};
-console.log(createCounter(10));
+const makeCounter = (n)=>{
+  let count = n;
+  return ()=>{
+    count++;
+    console.log(count);
+  }
+}
+
+let counter = makeCounter(10);
+counter();
+counter();
+counter();
